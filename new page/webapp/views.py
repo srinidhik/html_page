3db21ede639a1bd04f01fdab62acd53b5305028d
srinidhik <srinidhi.kambalapally@gmail.com>
2016-08-05 17:32:32 +0530

    with second page

--- new page/webapp/views.py
+++ new page/webapp/views.py
@@ -1,6 +1,10 @@
 from webapp import app
 from flask import render_template
 
-@app.route('/')
+@app.route('/1')
 def index():
     return render_template('layout.html')
+
+@app.route('/2')
+def index2():
+    return render_template('layout2.html')
